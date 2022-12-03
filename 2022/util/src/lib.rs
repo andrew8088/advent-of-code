770a8d41688e2f6ff2746693df104e703e9bcb3e
Andrew Burgess <andrew@burgess.io>
2022-12-03 07:04:34 -0500

    add rust util crate

---
+++ 2022/util/src/lib.rs
@@ -0,0 +1,34 @@
+use std::env;
+use std::fs;
+
+fn get_file_name() -> &'static str {
+    if env::var("DEMO").unwrap_or("0".to_string()) == "1" {
+        "demo-input.txt"
+    } else {
+        "input.txt"
+    }
+}
+
+pub fn get_input<F>(func: F)
+where
+    F: Fn(String),
+{
+    let file_name = get_file_name();
+    println!("loading {}", file_name);
+    match fs::read_to_string(file_name) {
+        Err(_) => println!("Cannot load file"),
+        Ok(content) => {
+            func(content);
+        }
+    }
+}
+
+pub fn get_lines<F>(func: F)
+where
+    F: Fn(Vec<&str>),
+{
+    get_input(|content| {
+        let lines: Vec<&str> = content.split('\n').collect();
+        func(lines)
+    })
+}
