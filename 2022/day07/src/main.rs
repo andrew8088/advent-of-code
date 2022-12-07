c4f65849ed8642a9153a32ef32f5e469d891fa6c
Andrew Burgess <andrew.burgess@classdojo.com>
2022-12-07 11:18:53 -0500

    day 7 2022 refactor

--- 2022/day07/src/main.rs
+++ 2022/day07/src/main.rs
@@ -1,8 +1,6 @@
 use std::collections::HashMap;
-
 use util::get_lines;
 use sscanf::sscanf;
-
 
 const TOTAL_DISK_SPACE: usize = 70_000_000;
 const REQUIRED_DISK_SPACE: usize = 30_000_000;
@@ -10,42 +8,19 @@ const REQUIRED_DISK_SPACE: usize = 30_000_000;
 fn main() {
     get_lines(|lines| {
         let mut pwd = Vec::new();
-        let mut dirs = Vec::new();
-        let mut files: HashMap<String, usize> = HashMap::new();
+        let mut dir_sizes: HashMap<String, usize> = HashMap::new();
 
         for line in lines { 
-            let clean_line = line.trim();
-
-            if clean_line.is_empty() {
-                continue;
-            }
-
-            let parsed: Result<&str, _> = sscanf!(clean_line, "$ cd {}", str);
-
-            match parsed {
-                Ok(dir) => {
-                    match dir {
-                        "/" => pwd.push(""),
-                        ".." => { pwd.pop(); },
-                        _ => pwd.push(dir),
-                    }
-                },
-                Err(_) => {
-                    if clean_line == "$ ls" {
-                        continue;
-                    }
-
-                    match sscanf!(clean_line, "{} {}", usize, str) {
-                        Ok((size, filename)) => {
-                            files.insert(format!("{}/{}", pwd.join("/"), filename), size);
-                        },
-                        Err(_) => {
-                            match sscanf!(clean_line, "dir {}", str) {
-                                Ok(dir) => dirs.push(format!("{}/{}", pwd.join("/"), dir)),
-                                Err(_) => panic!("unreachable: {}", clean_line),
-                            }
-                        },
-                    }
+            if let Ok(dir) = sscanf!(line, "$ cd {}", str) {
+                match dir {
+                    ".." => { pwd.pop(); },
+                    _ => pwd.push(dir),
+                }
+            } else if let Ok((size, _)) = sscanf!(line, "{} {}", usize, str) {
+                for i in 0..pwd.len() {
+                    let path = pwd[0..i+1].join("/");
+                    let curr_size = dir_sizes.get(&path).unwrap_or(&0);
+                    dir_sizes.insert(path, curr_size + size);
                 }
             }
         }
@@ -53,23 +28,15 @@ fn main() {
         let mut part1_sum = 0;
         let mut part2_min = REQUIRED_DISK_SPACE;
 
-        let unused_space: usize = TOTAL_DISK_SPACE - files.values().sum::<usize>();
+        let unused_space: usize = TOTAL_DISK_SPACE - dir_sizes.get("/").unwrap_or(&0);
 
-        for dir in dirs {
-            let mut dir_size = 0;
-            for (path, size) in &files {
-                if path.contains(&dir) {
-                    dir_size += size;
-                }
+        for (_, size) in dir_sizes {
+            if size < 100_000 {
+                part1_sum += size;
             }
 
-            if dir_size < 100_000 {
-                part1_sum += dir_size;
-            }
-
-            let empty_disk_space = dir_size + unused_space;
-            if empty_disk_space > REQUIRED_DISK_SPACE && dir_size < part2_min {
-                part2_min = dir_size;
+            if (size + unused_space) > REQUIRED_DISK_SPACE && size < part2_min {
+                part2_min = size;
             }
         }
 
