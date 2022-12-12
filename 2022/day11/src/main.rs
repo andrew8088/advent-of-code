23b68d3dc5b9c3cd8c9aaa667928f387be54eb57
Andrew Burgess <andrew@burgess.io>
2022-12-11 21:24:23 -0500

    wip

--- 2022/day11/src/main.rs
+++ 2022/day11/src/main.rs
@@ -1,36 +1,6 @@
+use num_bigint::{BigInt, Sign};
 use sscanf::sscanf;
 use util::get_lines;
-
-#[derive(Clone)]
-struct Monkey {
-    items: Vec<usize>,
-    operation: (Option<usize>, Option<usize>, char),
-    throw_to: (usize, usize, usize),
-}
-
-impl Monkey {
-    fn get_worry(&self, worry: usize) -> usize {
-        let (left, right, op) = self.operation;
-
-        match op {
-            '+' => left.unwrap_or(worry) + right.unwrap_or(worry),
-            '-' => left.unwrap_or(worry) - right.unwrap_or(worry),
-            '*' => left.unwrap_or(worry) * right.unwrap_or(worry),
-            '/' => left.unwrap_or(worry) / right.unwrap_or(worry),
-            _ => panic!("unsupported operator: {}", op),
-        }
-    }
-
-    fn next(&self, worry: usize) -> usize {
-        let (divisor, monkey_true, monkey_false) = self.throw_to;
-
-        if worry % divisor == 0 {
-            monkey_true
-        } else {
-            monkey_false
-        }
-    }
-}
 
 fn main() {
     get_lines(|lines| {
@@ -43,45 +13,114 @@ fn main() {
                 throw_to: get_throw_to(&monkey[3..]),
             })
             .collect::<Vec<Monkey>>();
-        let mut all_items = monkeys.iter().map(|m| m.items.clone()).collect::<Vec<_>>();
 
-        for round in 1..21 {
-            println!("Round {}", round);
+        let part1 = solve(20, &monkeys, true);
+        println!("part 1: {part1}");
 
-            for idx in 0..monkeys.len() {
-                // println!("monkey {idx}");
-                let m = &monkeys[idx];
-                let items = all_items[idx].clone();
-                for worry in items {
-                    let new_worry = m.get_worry(worry) / 3;
-                    let next = m.next(new_worry);
-                    // println!("{} => {} (throw to {})", worry, new_worry, next,);
-                    all_items[next].push(new_worry);
-                }
-                all_items[idx] = Vec::new();
-            }
-
-            for idx in 0..all_items.len() {
-                println!("Monkey {idx}: {:?}", all_items[idx]);
-            }
-        }
+        let part2 = solve(21, &monkeys, false);
+        println!("part 2: {part2}");
     })
 }
 
-fn get_items(line: &str) -> Vec<usize> {
+fn solve(rounds: usize, monkeys: &Vec<Monkey>, do_divide: bool) -> BigInt {
+    let mut all_items = monkeys.iter().map(|m| m.items.clone()).collect::<Vec<_>>();
+    let mut item_count = vec![0; all_items.len()];
+
+    for idx in 1..(rounds + 1) {
+        do_round(&monkeys, &mut all_items, &mut item_count, do_divide);
+
+        if idx == 1 || idx == 20 || idx % 1000 == 0 {
+            println!("==== Round {idx} ====");
+            // for idx in 0..monkeys.len() {
+            //     println!("Monkey {idx}: {:?}", all_items[idx]);
+            // }
+            for (idx, count) in item_count.iter().enumerate() {
+                println!("Monkey {idx} - {count} items");
+            }
+        }
+    }
+
+    item_count.sort();
+    item_count
+        .iter()
+        .rev()
+        .take(2)
+        .fold(big_int(1), |a, b| a * b)
+}
+
+#[derive(Clone)]
+struct Monkey {
+    items: Vec<BigInt>,
+    operation: (Option<BigInt>, Option<BigInt>, char),
+    throw_to: (usize, usize, usize),
+}
+
+impl Monkey {
+    fn get_worry(&self, worry: &BigInt, do_divide: bool) -> BigInt {
+        let (left, right, op) = self.operation;
+        // let divisor = self.throw_to.0;
+
+        let val: BigInt = match op {
+            '+' => left.unwrap_or(*worry) + right.unwrap_or(*worry),
+            '-' => left.unwrap_or(*worry) - right.unwrap_or(*worry),
+            '*' => left.unwrap_or(*worry) * right.unwrap_or(*worry),
+            '/' => left.unwrap_or(*worry) / right.unwrap_or(*worry),
+            _ => panic!("unsupported operator: {}", op),
+        };
+
+        if do_divide {
+            val / 3
+        } else {
+            val
+        }
+    }
+
+    fn next(&self, worry: &BigInt) -> usize {
+        let (divisor, monkey_true, monkey_false) = self.throw_to;
+
+        if worry % divisor == big_int(0) {
+            monkey_true
+        } else {
+            monkey_false
+        }
+    }
+}
+
+fn do_round(
+    monkeys: &Vec<Monkey>,
+    all_items: &mut Vec<Vec<BigInt>>,
+    item_count: &mut Vec<usize>,
+    do_divide: bool,
+) {
+    for idx in 0..monkeys.len() {
+        let m = &monkeys[idx];
+        let items = all_items[idx].clone();
+        item_count[idx] += items.len();
+        for worry in items {
+            let new_worry = m.get_worry(&worry, do_divide);
+            let next = m.next(&new_worry);
+            all_items[next].push(new_worry);
+        }
+        all_items[idx] = Vec::new();
+    }
+}
+
+fn get_items(line: &str) -> Vec<BigInt> {
     line.split(": ")
         .last()
         .expect("no starting items found")
         .split(",")
         .map(|x| {
-            x.trim()
-                .parse::<usize>()
-                .expect("could not parse item as usize")
+            big_int(
+                x.trim()
+                    .parse::<usize>()
+                    .expect("could not parse item as usize"),
+            )
         })
         .collect()
 }
 
-fn get_operation(line: &str) -> (Option<usize>, Option<usize>, char) {
+fn get_operation(line: &str) -> (Option<BigInt>, Option<BigInt>, char) {
     match sscanf!(line, "  Operation: new = {} {} {}", str, char, str) {
         Ok((left, op, right)) => {
             let left = get_op_val(left);
@@ -93,11 +132,11 @@ fn get_operation(line: &str) -> (Option<usize>, Option<usize>, char) {
     }
 }
 
-fn get_op_val(arg: &str) -> Option<usize> {
+fn get_op_val(arg: &str) -> Option<BigInt> {
     if arg == "old" {
         None
     } else {
-        Some(arg.parse::<usize>().expect("{} not an usize"))
+        Some(big_int(arg.parse::<usize>().expect("{} not an usize")))
     }
 }
 
@@ -110,12 +149,9 @@ fn get_throw_to(lines: &[&str]) -> (usize, usize, usize) {
         .expect("no false monkey found");
 
     (divisor, monkey_true, monkey_false)
+}
 
-    // move |value: usize| {
-    //     if value % divisor == 0 {
-    //         monkey_true
-    //     } else {
-    //         monkey_false
-    //     }
-    // }
+fn big_int(n: usize) -> BigInt {
+    let bytes = n.to_le_bytes();
+    BigInt::from_bytes_le(Sign::Plus, &bytes)
 }
