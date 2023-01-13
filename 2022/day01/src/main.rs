76deb1f1d83a44f0479b37ab151ecbb77a2b6d31
Andrew Burgess <andrew@burgess.io>
2023-01-12 21:28:49 -0500

    cleanup

--- 2022/day01/src/main.rs
+++ 2022/day01/src/main.rs
@@ -4,18 +4,21 @@ fn main() {
     match fs::read_to_string("input.txt") {
         Err(_) => println!("Cannot load file"),
         Ok(content) => {
-            let counts = content.split("\n\n").map(|calorie_rows| -> usize {
-                calorie_rows.split('\n').map(|l| l.parse::<usize>().unwrap_or(0)).sum()
+            let counts = content.split("\n\n").map(|rows| -> usize {
+                rows.split('\n').map(|row| row.parse().unwrap_or(0)).sum()
             });
 
-            let mut v: Vec<usize> = counts.collect();
-
-            let len = v.len();
+            let mut v: Vec<_> = counts.collect();
 
             v.sort();
 
-            let top_three: usize = v.split_off(len - 3).into_iter().sum();
-            println!("sum of top 3 calorie counts: {:?}", top_three);
+            let last_idx = v.len() - 1;
+            println!("the highest count is {}", v[last_idx]);
+
+            // let len = v.len();
+
+            // let top_three: usize = v.split_off(len - 3).into_iter().sum();
+            // println!("sum of top 3 calorie counts: {:?}", top_three);
         }
     }
 }
